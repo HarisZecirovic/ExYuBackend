3b907ca154dd072737a15dcddb03438979193bf2
Haris <hary_z@live.com>
2026-04-25 12:25:46 +0200

    Initial commit

---
+++ src/main/java/com/example/exyubackend/ExYuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.exyubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExYuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExYuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exyubackend/ExYuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exyubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExYuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
